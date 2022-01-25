fc8ee7dc20a9ba146329f61b8e91cb93ffb68ea7
samhodgson88 <sam.hodgson.coding@gmail.com>
2022-01-25 15:01:57 +0000

    added buttons

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const Modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const OpenModal = function () {
+    Modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+    Modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+    btnsOpenModal[i].addEventListener('click',
+        OpenModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+
+
+
+
+
+
+
+
+
